df04c11c1204cc085eaf1cd4ad2a1f102fdc1535
LEE-GyeongMin <min20@nhnnext.org>
2015-02-10 01:36:57 +0900

    final commit

--- ui-dev/js/sg-timeline.js
+++ ui-dev/js/sg-timeline.js
@@ -43,12 +43,32 @@
 				});
 		};
 		
-				
+		// TEST
+		// 학생으로 로그인 되어있을 시 FORBIDDEN
 		$http.get(_apiRoot + __classId + "/registrations").
 			success(function(data) {
 				console.log(data);
 			});
-
+		
+		// item 정보. 이거 써먹을 수 있도록 페이지 만들어야.
+		$http.get(_apiRoot + __classId + "/1").
+			success(function(data) {
+				console.log(data);
+			});
+		
+		// assignment 정보
+		$http.get(_apiRoot + __classId + "/2/submissions").
+			success(function(data) {
+				console.log(data);
+			});
+			
+		// class 정보
+		$http.get(_apiRoot + __classId).
+			success(function(data) {
+				console.log(data);
+			});
+		// /TEST
+		
 		var __regExp = new RegExp("/[0-9]+");
 		$rootScope.$watch("currentUser", function(newUser) {
 			if (newUser) {
@@ -57,7 +77,64 @@
 				}
 			}
 		});
+		
+		$scope.prof = {};
+		$scope.prof.activeTab = "alert";
+		$scope.prof.isSelected = function(tabName) {
+			return $scope.prof.activeTab == tabName;
+		};
+		$scope.prof.activate = function(tabName) {
+			$scope.prof.activeTab = tabName;
+		};
+		
+		$scope.writeAlert = function(text) {
+			$http.post(_apiRoot + __classId + "/timeline", {
+				itemType: "alert",
+				data: {
+					text: text
+				}
+			}).success(function() {
+				__getTimelineItems()
+			});
+		}
+		
+		$scope.writeLecture = function(title) {
+			$http.post(_apiRoot + __classId + "/timeline", {
+				itemType: "lecture",
+				data: {
+					title: title,
+					attachments: []
+				}
+			}).success(function() {
+				__getTimelineItems()
+			});
+		}
 
+		$scope.writeAssignment = function(title, description, dueDate) {
+			var longDueDate = new Date(dueDate).getTime();
+			console.log({
+				itemType: "assignment",
+				data: {
+					title: title,
+					description: description,
+					due_datetime: longDueDate,
+					attachments: []
+				}
+			});
+
+			$http.post(_apiRoot + __classId + "/timeline", {
+				itemType: "assignment",
+				data: {
+					title: title,
+					description: description,
+					due_datetime: longDueDate,
+					attachments: []
+				}
+			}).success(function() {
+				__getTimelineItems()
+			});
+		}
+				
 		// autosize on textarea
 		$("textarea").autosize();
 	}]);
